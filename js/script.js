629a2eeed3e95002ed98b8454b82e9758637eed8
LucioGhedo <lucioghedina@gmail.com>
2022-06-15 14:36:40 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,111 @@
+// html elements
+const mainGrid = document.getElementById('main-grid')
+const playButton = document.getElementById('play')
+const clickedNumbers = [];
+const userMessageDiv = document.querySelector('#user-message');
+
+// array
+let bombs = [];
+
+playButton.addEventListener('click', 
+function() {
+    const userLevel = document.getElementById('user-level').value
+    
+    if (userLevel === 'easy') {
+        gameMaxRange = 100;
+        bombs = generateBombs(16, 1, 100)
+        gamesContinue = true;
+        createGrid(gameMaxRange, 'easy');
+    } else if (userLevel === 'medium') {
+        gameMaxRange = 81;
+        bombs = generateBombs(16, 1, gameMaxRange)
+        gamesContinue = true;
+        createGrid(gameMaxRange, 'medium');
+    } else if (userLevel === 'crazy') {
+        gameMaxRange = 49;
+        bombs = generateBombs(16, 1, gameMaxRange)
+        gamesContinue = true;
+        createGrid(gameMaxRange, 'crazy');
+    } else {
+        alert('refresh page e inserisci un numero valido')
+        gamesContinue = false;
+    }
+}
+)
+
+// FUNZIONI
+
+// genera un array di 16 bombe tutte diverse
+// return: array con numeri random tutti diversi con lunghezza numOfElem
+// rangeMin, rangeMax sono il range degli elementi
+function generateBombs(numOfElem, rangeMin, rangeMax) {
+    let randomNumArray = [];
+    while(randomNumArray.length < numOfElem) {
+        const randomNum = getRndInteger(rangeMin, rangeMax)
+        if(!randomNumArray.includes(randomNum)) {
+            randomNumArray.push(randomNum)
+        }
+    }
+    console.log(randomNumArray)
+    return randomNumArray
+}
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min +1) ) + min;
+}
+function createGrid(gameMaxRange, userLevel) {
+    for(let i = 1; i <= gameMaxRange; i++) {
+        // Creare degli square
+        // <div class="square"><span>12</span></div>
+        const newSquare = document.createElement('div');
+        // popolare il numero
+        newSquare.innerHTML = `<span>${i}</span>`;
+        // Aggiungere la classe square
+        newSquare.classList.add('square', userLevel);
+    
+        // Aggiungere cosa succede al click
+        
+        // Appenderlo
+        mainGrid.append(newSquare);
+    }
+    let newSquareF = document.querySelectorAll('.square')
+    for(let i = 0; i < newSquareF.length; i++) {
+        let squares = newSquareF[i]
+        if (!bombs.includes(manageSquareClick)) {
+            squares.addEventListener('click', manageSquareClick);
+        } else if (bombs.includes(manageSquareClick)) {
+            alert('hai perso')
+            newSquareF.removeEventListener(manageSquareClick)
+        }
+    }
+    
+}
+function manageSquareClick() {
+    // Milestone 2
+    // Mi prendo il numero dentro lo span che è figlio dell'elemento cliccato
+    // Leggo lo span
+    const thisNumber = parseInt(this.querySelector('span').innerHTML);
+    
+    if(bombs.includes(thisNumber)) {
+        endGame()
+        this.classList.add('red');
+        document.getElementById('main-grid').removeEventListener('click', manageSquareClick)
+    } else {
+        this.classList.add('blue');
+    }
+
+    // Pushare il numero nell'array dei numeri selezionati
+    clickedNumbers.push(thisNumber);
+    console.log(clickedNumbers);
+
+    // Se l'utente ha cliccato su tutti i numeri
+    // Scrivo il messaggio di fine gioco
+    if(clickedNumbers.length === gameMaxRange - 16) {
+        endGame()
+    }
+
+    // Quando ho finito le mia operazioni la cella non è più cliccabile
+    this.style.pointerEvents = 'none';
+}
+function endGame() {
+    userMessageDiv.innerHTML = `Hai finito il gioco, l'ordine dei click è: ${clickedNumbers}`;
+}
